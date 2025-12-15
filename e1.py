86051897bd2463207afe1b3529eb3a79760e76cd
bharathseepana <bharathseepana431@gmail.com>
2025-12-15 14:49:39 +0530

    Add files via upload

---
+++ e1.py
@@ -0,0 +1,56 @@
+a=10
+b=12
+print(a+b)
+
+numbers = [10, 20, 30, 40]
+print(numbers)
+
+  #for loop
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])
+
+
+  #variables
+student = {
+    "name": "Ravi",
+    "age": 20,
+    "branch": "CSE"
+}
+print(student)
+
+  #variables2
+num = 5
+
+if num > 0:
+    print("True")
+else:
+    print("False")
+
+
+#integers
+a = 10
+b = 3
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a // b)  # Integer division
+print(a % b)   # Modulus
+
+
+#do loop
+i = 1
+
+while True:
+    print(i)
+    i += 1
+    if i > 5:
+        break
+
+#while loop
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+    
